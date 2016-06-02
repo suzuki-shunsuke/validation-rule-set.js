77891f4ab5c9aaa74471dde5042a51fdb43c996f
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2016-06-03 08:04:24 +0900

    Node.jsでのテストの修正
    npm run test:node でテストが実行される

--- test/node/src/main.js
+++ test/node/src/main.js
@@ -3,10 +3,10 @@
 const chai = require('chai');
 const mocha = require('mocha');
 
-const rule_set = require('../../src/main');
+const rule_set = require('../../../src/main.js');
 
 mocha.describe('rule_set.boolean', () => {
   mocha.it('', () => {
-    chai.expect(rule_set.boolean('f')).to.be.false;
+    chai.expect(rule_set.bool('f')).to.be.false;
   });
 });
